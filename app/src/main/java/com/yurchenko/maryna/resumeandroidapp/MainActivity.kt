05638fe17e2b6b205c7eb2c19743d049e2a36e96
yurchenkomaryna <yurchenko.maryna@gmail.com>
2018-07-28 22:55:56 -0700

    last modified files for the project

--- app/src/main/java/com/yurchenko/maryna/resumeandroidapp/MainActivity.kt
+++ app/src/main/java/com/yurchenko/maryna/resumeandroidapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.yurchenko.maryna.resumeandroidapp
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -22,9 +23,24 @@ class MainActivity : AppCompatActivity() {
 
         var callMaryna = findViewById<Button>(R.id.callMaryna)
 
-        callMaryna.setOnClickListener{
+        callMaryna.setOnClickListener {
             println("This is my phone number")
+
+            var phoneUri = Uri.parse("tel:1800940525")
+            var callIntent = Intent(Intent.ACTION_DIAL, phoneUri)
+            startActivity(callIntent)
+        }
+        var emailButton = findViewById<Button>(R.id.emailButton)
+
+        emailButton.setOnClickListener{
+             println("Email")
+
+            var emailIntent = Intent(Intent.ACTION_SEND)
+            emailIntent.setType("plain/text")
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, "yurchenko.maryna@gmail.com")
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Nica Resume")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "I really enjoyed your resume")
+            startActivity(emailIntent)
         }
     }
-
 }
